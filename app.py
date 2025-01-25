d91efafad035bbc2069fd78851f50aff3f6e7a63
DHYEY166 <97232546+DHYEY166@users.noreply.github.com>
2025-01-24 17:45:45 -0800

    final

--- app.py
+++ app.py
@@ -226,6 +226,9 @@ def report_page():
 
 if __name__ == '__main__':
     with app.app_context():
-        db.create_all()  # Creates the database and tables
+        db.create_all()
     app.run(host='0.0.0.0', port=5000, debug=True)
+
+    
+
 
